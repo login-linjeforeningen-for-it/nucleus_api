fdf22c867170c5c74c6dc8913dbff1c9206a9f54
eirikhanasand <eirik.hanasand@gmail.com>
2026-04-28 04:31:34 +0200

    Inline game catalog

--- src/handlers/games.ts
+++ src/handlers/games.ts
@@ -1,8 +1,37 @@
 import type { FastifyReply, FastifyRequest } from 'fastify'
-import games from '../games/games.ts'
+import { categories as neverhaveieverCategories } from '../games/neverhaveiever.ts'
 import neverhaveiever from '../games/neverhaveiever.ts'
+import { categories as okredflagdealbreakerCategories } from '../games/okredflagdealbreaker.ts'
 import okredflagdealbreaker from '../games/okredflagdealbreaker.ts'
+import { categories as questionCategories } from '../games/questions.ts'
 import questions from '../games/questions.ts'
+
+const games = [
+    {
+        id: 0,
+        name: '100 Questions',
+        endpoint: '/questions',
+        description_no: 'Det klassiske 100 spørsmål / snusboks / pekelek spillet.',
+        description_en: 'The classic 100 questions / snusbox / pointing game.',
+        categories: questionCategories
+    },
+    {
+        id: 1,
+        name: 'Never Have I Ever',
+        endpoint: '/neverhaveiever',
+        description_no: 'Jeg har aldri... Drikk hvis du har.',
+        description_en: 'Never have I ever... Drink if you have.',
+        categories: neverhaveieverCategories
+    },
+    {
+        id: 2,
+        name: 'Ok Red Flag Dealbreaker',
+        endpoint: '/okredflagdealbreaker',
+        description_no: 'Spillet hvor man får en påstand og skal svare om det er ok, red flag eller dealbreaker.',
+        description_en: 'The game where you get a statement and have to answer if it\'s ok, red flag or dealbreaker.',
+        categories: okredflagdealbreakerCategories
+    },
+]
 
 export async function getGames(_: FastifyRequest, res: FastifyReply) {
     res.send(games)
